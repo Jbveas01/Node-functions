bdbc526baa28194c396fee8facf17a1e2f31fab3
Jacob Veasey <jbveas01@gmail.com>
2022-07-07 08:33:08 -0400

    Added helper2 testing dependencies in Azure

--- postNodeAligner/index.js
+++ postNodeAligner/index.js
@@ -6,7 +6,7 @@ module.exports = async function (context, req) {
 
     const name = (req.query.name || (req.body && req.body.name));
     const responseMessage = name
-        ? "Hello, " + name + ". This HTTP triggered function executed successfully." + variables.appName
+        ? "Hello, " + name + ". This HTTP triggered function executed successfully." + variables.appName + variables.house
         : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
 
     context.res = {
--- utils/helper.js
+++ utils/helper.js
@@ -1,4 +1,7 @@
+const house = require('./helper2')
+
 module.exports = {
     appName: 'Jacob',
-    currentVersion: 1
+    currentVersion: 1,
+    house: house.house
 }
